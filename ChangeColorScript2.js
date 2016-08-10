3753cdb2d2d019d912c540e1f7dc4b8460a29e56
NeiloRV <rvneilo@gmail.com>
2016-08-10 15:33:45 +0300

    first variant of html and js task

---
+++ ChangeColorScript2.js
@@ -0,0 +1,9 @@
+function changeColor(){
+if(document.bgColor == "green"){
+    document.bgColor = "blue";}
+else{
+    if(document.bgColor == "blue"){
+      document.bgColor = "red";
+    } else {document.bgColor = "green";}
+}
+}
